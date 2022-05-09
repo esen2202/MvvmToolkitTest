b1d9f98a885ebfac2fdb24394c8c5dc944d33d41
erkan esen <erkanesen2202@hotmail.com>
2022-05-09 23:01:01 +0300

    Add project files.

---
+++ MvvmToolkitTest/MainWindowModelView.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmToolkitTest
+{
+    // [INotifyPropertyChanged]
+    public partial class MainWindowModelView : ObservableObject
+    {
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(Fullname))]
+        private string name;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(Fullname))]
+        private string surname;
+
+
+        public string Fullname => $"{Fullname} {Surname}";
+      
+        [ICommand]
+        public void Submit()
+        {
+            Console.WriteLine("Submit Clicked");
+        }
+
+    } 
+}
